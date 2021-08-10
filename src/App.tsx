efb05c3bfbfce0b0fc896862fda5e1e295812dce
Angelo Cordon <angelocordon@gmail.com>
2021-08-10 15:12:37 -0700

    Set up WindiCSS
    Add WindiCSS and basic styling.
    Co-authored-by: David Harris <david@roshreview.com>

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,29 @@ import "./App.css";
 import keyboardMap from "./lib/keyboard-map";
 
 function App() {
-	const [keyboardKey, setKeyboardKey] = useState("");
+  const [keyboardKey, setKeyboardKey] = useState("");
 
-	document.addEventListener("keydown", (e) => {
-		setKeyboardKey(e.key);
-	});
+  document.addEventListener("keydown", (e) => {
+    setKeyboardKey(e.key);
+  });
 
-	const renderKeyDictionary = () => {
-		if (!keyboardMap[keyboardKey]) {
-			return <p>{keyboardKey} does not have an associated vim command</p>;
-		}
+  const renderKeyDictionary = () => {
+    if (!keyboardMap[keyboardKey]) {
+      return <span>{keyboardKey} does not have an associated vim command</span>;
+    }
 
-		return (
-			<p>
-				{keyboardKey}: {keyboardMap[keyboardKey]}
-			</p>
-		);
-	};
+    return (
+      <span>
+        {keyboardKey}: {keyboardMap[keyboardKey]}
+      </span>
+    );
+  };
 
-	return <div className="App">{keyboardKey && renderKeyDictionary()}</div>;
+  return (
+    <div className="App h-full text-light-50 flex items-center justify-center">
+      <p className="text-5xl">{keyboardKey && renderKeyDictionary()}</p>
+    </div>
+  );
 }
 
 export default App;
