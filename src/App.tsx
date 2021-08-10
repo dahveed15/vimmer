924879478be2fd5cbe0895d3ef718cf38e08de38
Angelo Cordon <angelocordon@gmail.com>
2021-08-10 15:36:47 -0700

    Style main UI

--- src/App.tsx
+++ src/App.tsx
@@ -10,19 +10,39 @@ function App() {
 
   const renderKeyDictionary = () => {
     if (!keyboardMap[keyboardKey]) {
-      return <span>{keyboardKey} does not have an associated vim command</span>;
+      return (
+        <span className="flex items-center">
+          <strong className="text-xl mr-2 border border-emerald-500 py-1 px-3 rounded">
+            {keyboardKey}
+          </strong>
+          does not have an associated vim command
+        </span>
+      );
     }
 
     return (
-      <span>
-        {keyboardKey}: {keyboardMap[keyboardKey]}
+      <span className="flex items-center">
+        <strong className="text-xl mr-2 border border-emerald-500 py-1 px-3 rounded">
+          {keyboardKey}
+        </strong>
+        {keyboardMap[keyboardKey]}
       </span>
     );
   };
 
   return (
     <div className="App h-full text-light-50 flex items-center justify-center">
-      <p className="text-5xl">{keyboardKey && renderKeyDictionary()}</p>
+      {keyboardKey.length > 0 ? (
+        <div>{renderKeyDictionary()}</div>
+      ) : (
+        <div>
+          <h1 className="text-5xl mb-4">
+            <span className="text-emerald-500">Vim</span>
+            <span>mer</span>
+          </h1>
+          <p>Press a keyboard key to find what it maps to in Vim.</p>
+        </div>
+      )}
     </div>
   );
 }
